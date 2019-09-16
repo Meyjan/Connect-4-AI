5b578cd288604f83734de67a5e8962ef334e3031
Meyjan <saragih.meyer0821@gmail.com>
2019-09-16 15:30:16 +0700

    printBoard method

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 board = [0] * 42
 
+
 def fill(number, side):
     i = number - 1
     while i < len(board):
@@ -12,19 +13,25 @@ def fill(number, side):
         else:
             i += 7
 
+
+def printBoard():
+    # board[choice-1] = 1
+    init = 35
+    for i in range(len(board)):
+        print(board[init+i], end='')
+        if(i % 7 == 6):
+            print()
+            init = init - 14
+
+
 def main():
     side = -1
     while True:
         side = -side
         choice = int(input())
         fill(choice, side)
-        # board[choice-1] = 1
-        init = 35
-        for i in range(len(board)):
-            print(board[init+i], end = '')
-            if(i % 7 == 6):
-                print()
-                init = init - 14
+        printBoard()
+
 
 if __name__ == "__main__":
     main()
